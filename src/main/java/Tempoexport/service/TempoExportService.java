de5ff6236105bba9cd35d1baf008857316277ac2
Markko Junolainen <markko.junolainen@gmail.com>
2020-12-15 12:45:50 +0200

    .

--- src/main/java/Tempoexport/service/TempoExportService.java
+++ src/main/java/Tempoexport/service/TempoExportService.java
@@ -1,15 +1,21 @@
 package Tempoexport.service;
 
-import Tempoexport.repository.TempoExportRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import Tempoexport.repository.TempoExportRepository;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class TempoExportService {
 
     @Autowired
-     TempoExportRepository tempoExportRepository;
+    private TempoExportRepository tempoExportRepository;
+    private RestTemplate restTemplate;
 
     @Value("fcg42Im2JmExadbA31ZaWHTFzlK5Ll")
     private String bearer;
@@ -17,5 +23,13 @@ public class TempoExportService {
     @Value("https://api.tempo.io/core/3/worklogs")
     private String address;
 
+    /*curl --location --request GET “https://api.tempo.io/core/3/worklogs” \
+    --header “Authorization: Bearer fcg42Im2JmExadbA31ZaWHTFzlK5Ll” \*/
 
+    public void tempoData() {
+        UriComponentsBuilder url = UriComponentsBuilder.fromHttpUrl(address)
+                .queryParam("bearer", bearer);
+        Map response = restTemplate.getForObject(url.toUriString(), HashMap.class);
+
+    }
 }
